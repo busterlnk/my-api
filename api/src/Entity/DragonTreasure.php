841077be6dc015bb87b5ee3721944cbd10281f63
franco.sosa <franco.sosa@conjurer.com>
2023-12-07 16:01:45 +0100

    Nuevas pruebas de apiPlatform

--- api/src/Entity/DragonTreasure.php
+++ api/src/Entity/DragonTreasure.php
@@ -2,15 +2,47 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Serializer\Filter\PropertyFilter;
 use App\Repository\DragonTreasureRepository;
+use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
+use function Symfony\Component\String\u;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
 #[ApiResource(
-    description: 'descripcion de una tabla de pruebas'
+    description: 'descripcion de una tabla de pruebas',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+    ],
+    shortName: 'Treasure',
+    normalizationContext: [
+        'groups' => ['treasure:read']
+    ],
+    denormalizationContext: [
+        'groups' => ['treasure:write']
+    ],
+    paginationItemsPerPage: 10
 )]
+#[ApiFilter(PropertyFilter::class)]
 class DragonTreasure
 {
     #[ORM\Id]
@@ -19,22 +51,41 @@ class DragonTreasure
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['treasure:read', 'treasure:write'])]
+    #[ApiFilter(SearchFilter::class, strategy: 'partial')]
+    #[Assert\NotBlank]
+    #[Assert\Lenght(min: 2, max: 50, maxMessage: 'Describe your loot in 50 characters or less')]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups('treasure:read')]
+    #[ApiFilter(SearchFilter::class, strategy: 'partial')]
+    #[Assert\NotBlank]
     private ?string $description = null;
 
     #[ORM\Column]
-    private ?int $value = null;
+    #[Groups(['treasure:read', 'treasure:write'])]
+    #[ApiFilter(RangeFilter::class)]
+    #[Assert\GreaterThanOrEqual(0)]
+    private ?int $value = 0;
 
     #[ORM\Column]
-    private ?int $coolFactor = null;
+    #[Groups(['treasure:read', 'treasure:write'])]
+    #[Assert\GreaterThanOrEqual(0)]
+    #[Assert\LessThanOrEqual(0)]
+    private ?int $coolFactor = 0;
 
     #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
+    private ?\DateTimeImmutable $createdAt;
 
     #[ORM\Column]
-    private ?bool $isPublished = null;
+    #[ApiFilter(BooleanFilter::class)]
+    private bool $isPublished = false;
+
+    public function __construct(string $name = null){
+        $this->name = $name;
+        $this->createdAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -46,21 +97,30 @@ class DragonTreasure
         return $this->name;
     }
 
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
-    }
 
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
+    #[Groups('treasure:read')]
+    public function getShortDescription(): ?string
+    {
+        return u($this->description)->truncate(40, '...');
+    }
+
     public function setDescription(string $description): static
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    #[Groups('treasure:write')]
+    #[SerializedName('description')]
+    public function setTextDescription(string $description): static
+    {
+        $this->description = nl2br($description);
 
         return $this;
     }
@@ -94,7 +154,18 @@ class DragonTreasure
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+
+
+    /**
+     * @return A human-readable representation of when this treasure was plundered.
+     */
+    #[Groups('treasure:read')]
+    public function getCreatedAtAgo(): string
+    {
+        return Carbon::instance($this->createdAt)->diffForHumans();
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
     {
         $this->createdAt = $createdAt;
 
